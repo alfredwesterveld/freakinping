bfe6a2eeb7e1e2ede84b764cd4d1f42f46754f5b
Alfred Westerveld <alfredwesterveld@gmail.com>
2011-01-18 01:38:20 +0100

    Added cli argument for interval

--- interval.js
+++ interval.js
@@ -2,7 +2,16 @@ var redis = require("redis"),
     client = redis.createClient();
 
 //var delay = 5 * 60 * 1000; // 5 minutes
-var delay = 30 * 1000; // 5 seconds
+var delay;
+
+if (process.argv[2]) {
+    delay = process.argv[2] * 1000;    
+} else {
+    console.log('interval argument missing');
+    process.exit();
+}
+
+console.log(delay);
 
 var callback = function() {
     console.log(new Date().toLocaleTimeString());
